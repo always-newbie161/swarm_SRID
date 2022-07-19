a166a4f01b6d6bd3511b7ee2831cbf2bbfc2b3d9
Srikar Jilugu <66471669+always-newbie161@users.noreply.github.com>
2022-07-19 22:09:00 +0530

    Update broadcast_utils.js

--- broadcast_utils.js
+++ broadcast_utils.js
@@ -1,42 +1,20 @@
-var port_assigned  = process.argv[2];
-var STATE = process.argv[3];
-var schedule_id = process.argv[4];
-
 var dgram = require('dgram');
 var device_utils = require('./device_utils');
 
-function state_broadcast(device_id, STATE, SWARM_PORTS){
-    var message = new device_utils.device_info(device_id, STATE);
-    
+
+function broadcast(device){
     // Create a udp socket client object.
     var client = dgram.createSocket("udp4");
-    message = JSON.stringify(message)
+    message = JSON.stringify(device)
 
-    for(port of SWARM_PORTS){
+    for(port of device.ALL_PORTS){
         client.send(message, 0, message.length, port, "localhost");
     }
-    console.log('UDP broadcast done successfully to all devices')
-}
-
-
-function grouping_broadcast(port_assigned, schedule_id, ALL_PORTS){
-    var message =  new device_utils.content_request(port_assigned, schedule_id);
-
-    // Create a udp socket client object.
-    var client = dgram.createSocket("udp4");
-    message = JSON.stringify(message)
-
-    for(port of ALL_PORTS){
-        client.send(message, 0, message.length, port, "localhost");
-    }
-    console.log('UDP broadcast done successfully to all devices')
-    
+    console.log(device.ALL_PORTS, 'UDP broadcast done successfully to all devices')
     
 }
 
 module.exports = {
-    state_broadcast,
-    grouping_broadcast,
+    broadcast,
 };
 
-grouping_broadcast(port_assigned, schedule_id);
