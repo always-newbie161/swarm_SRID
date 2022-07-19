8b244cc0107c3f3427fcdd004eb3560e3d3befcc
Srikar Jilugu <66471669+always-newbie161@users.noreply.github.com>
2022-07-19 22:09:20 +0530

    Update device_utils.js

--- device_utils.js
+++ device_utils.js
@@ -1,27 +1,15 @@
-function device_info(id, state){
-    this.id = id;
+function device_info(state=undefined, port_assigned, schedule_id, grouped=false, SWARM_PORTS=new Set(), ALL_PORTS=([])){
+    this.id = `device_${port_assigned}`;
     this.state = state;
+    this.port_assigned = port_assigned;
+    this.schedule_id = schedule_id;
+    this.SWARM_PORTS = SWARM_PORTS;
+    this.ALL_PORTS = ALL_PORTS;
+    this.grouped = grouped;
 };
 
-var port_assigned;
-var schedule_needed;
-var SWARM_PORTS;
-
-function content_request(device_id, schedule_id){
-    this.device_id = device_id;
-    this.schedule_id = schedule_id;
-}
-
-function update_swarm(port){
-    SWARM_PORTS.push(port);
-}
 
 module.exports = {
     device_info,
-    content_request,
-    port_assigned,
-    schedule_needed,
-    SWARM_PORTS,
-    update_swarm,
 };
   
